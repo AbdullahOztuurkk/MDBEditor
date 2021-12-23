35b99ec0deb621224022ff0a837f62dd0077c5bf
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-12-23 22:22:34 +0300

    refactor: method name has been shortened

--- Helpers/FormHelper.cs
+++ Helpers/FormHelper.cs
@@ -15,7 +15,7 @@ namespace MDBEditor.Helpers
         /// Get all system colors as pictureboxes to control.
         /// </summary>
         /// <param name="control">Any FlowLayoutPanel</param>
-        public static void Get_Colors_Into_The_Control(this FlowLayoutPanel control)
+        public static void Get_Colors(this FlowLayoutPanel control)
         {
             KnownColor[] colors = (KnownColor[])Enum.GetValues(typeof(KnownColor));
             foreach (KnownColor knowColor in colors)
--- Pages/MasterForm.cs
+++ Pages/MasterForm.cs
@@ -16,8 +16,8 @@ namespace MDBEditor
         public MasterForm()
         {
             InitializeComponent();
-            FLP_Colors.Get_Colors_Into_The_Control();
-            FLP_Text_Colors.Get_Colors_Into_The_Control();
+            FLP_Colors.Get_Colors();
+            FLP_Text_Colors.Get_Colors();
             Lbl_Page_Size.Text = PB_Drawing_Board.Width + " " + PB_Drawing_Board.Height;
             
             //Add grid control to drawing board

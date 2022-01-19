07cef58a277df4fb35cba17dc4f9c29a2fc4d4bd
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-01-20 01:21:15 +0300

    feat: Eraser tool has been created

---
+++ Tools/Abstract/IEraserTool.cs
@@ -0,0 +1,18 @@
+﻿using MDBEditor.Tools.Abstract.General;
+using MDBEditor.Tools.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDBEditor.Tools.Abstract
+{
+    public interface IEraserTool: ITool
+    {
+        public Point Loc { get; set; }
+        public Point LastPoint { get; set; }
+        public int Size { get; set; }
+    }
+}

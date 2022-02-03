25f63a46ee65618d0def1682e4b37b5262220b93
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-04 01:00:35 +0300

    feat: text tool has been improved

--- Tools/Abstract/ITextTool.cs
+++ Tools/Abstract/ITextTool.cs
@@ -12,7 +12,9 @@ namespace MDBEditor.Tools.Abstract
     {
         public Point Loc { get; set; }
         public Font Font { get; set; }
-        public short Size { get; set; }
+        public String Text { get; set; }
+        public float Size { get; }
+        public FontStyle FontStyle { get; }
         public Color ForeColor { get; set; }
         public Color SideColor { get; set; }
     }
--- Tools/Concrete/TextTool.cs
+++ Tools/Concrete/TextTool.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,13 +20,35 @@ namespace MDBEditor.Tools.Concrete
 
         public Point Loc { get; set; }
         public Font Font { get; set; }
-        public short Size { get; set; }
+        public String Text { get; set; }
+        public float Size => (float)Font.Size;
+
+        public FontStyle FontStyle => Font.Style;
         public Color ForeColor { get; set; }
         public Color SideColor { get; set; }
 
         public void Handle()
         {
-            throw new NotImplementedException();
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            GraphicsPath path = new GraphicsPath(FillMode.Alternate);
+
+            using (FontFamily fontFamily = new FontFamily(Font.Name))
+            {
+                using (StringFormat sf = new StringFormat())
+                {
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Center;
+                    path.AddString(this.Text, fontFamily,
+                        (int)this.FontStyle, this.Size,
+                        this.Loc, sf);
+                }
+            }
+
+            graphics.FillPath(new SolidBrush(ForeColor), path);
+            using (Pen pen = new Pen(SideColor))
+            {
+                graphics.DrawPath(pen, path);
+            }
         }
     }
 }

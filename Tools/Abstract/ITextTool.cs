3bb99572032e45984b0f0762dcdb7330b3cedf0b
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-03 15:49:52 +0300

    feat: added text and filler tools but it needs to improvement.

---
+++ Tools/Abstract/ITextTool.cs
@@ -0,0 +1,19 @@
+﻿using MDBEditor.Tools.Abstract.General;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDBEditor.Tools.Abstract
+{
+    public interface ITextTool:ITool
+    {
+        public Point Loc { get; set; }
+        public Font Font { get; set; }
+        public short Size { get; set; }
+        public Color ForeColor { get; set; }
+        public Color SideColor { get; set; }
+    }
+}

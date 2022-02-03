d2f49f66ac1fee00acb2549cf12b1f927350cbbf
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-02-03 15:48:48 +0300

    feat: added tools to menu

--- Pages/MasterForm.cs
+++ Pages/MasterForm.cs
@@ -1,11 +1,10 @@
-﻿using MDBEditor.Constants;
-using MDBEditor.Constants.Enums;
+﻿using MDBEditor.Constants.Enums;
 using MDBEditor.Controls;
 using MDBEditor.Helpers;
 using MDBEditor.Tools.Concrete;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
-using System.Reflection;
 using System.Windows.Forms;
 
 namespace MDBEditor
@@ -13,16 +12,30 @@ namespace MDBEditor
     public partial class MasterForm : Form
     {
         private readonly PictureBoxWithGrid BoxWithGrid;
-        private PenTool penTool;
+
+        //Mouse event properties
         bool IsMouseDown = false;
         private Point lastPoint;
+
+        //Colors
+        private Color primaryColor = Color.Black;
+        private Color secondaryColor = Color.White;
+
+        //Tools
+        public DrawingTool currentTool;
+        private readonly PenTool penTool;
+        private readonly EraserTool eraserTool;
+        private ColorPickerTool colorPickerTool;
+        private readonly FillerTool fillerTool;
+        private readonly TextTool textTool;
+
         public MasterForm()
         {
             InitializeComponent();
             FLP_Colors.Get_Colors();
             FLP_Text_Colors.Get_Colors();
             Lbl_Page_Size.Text = PB_Drawing_Board.Width + " " + PB_Drawing_Board.Height;
-            
+
             //Add grid control to drawing board
             BoxWithGrid = new PictureBoxWithGrid(PB_Drawing_Board.Size);
             PB_Drawing_Board.Controls.Add(BoxWithGrid);
@@ -33,7 +46,13 @@ namespace MDBEditor
             //Add rulers to pictureboxes
             PB_Ruler_Left.Controls.Add(new RulerPictureBox(BoxAlignment.Vertical));
             PB_Ruler_Top.Controls.Add(new RulerPictureBox());
+
+            //Tools
             penTool = new PenTool(PB_Drawing_Board.CreateGraphics());
+            eraserTool = new EraserTool(PB_Drawing_Board.CreateGraphics());
+            fillerTool = new FillerTool(new Bitmap(PB_Drawing_Board.Width,PB_Drawing_Board.Height));
+            colorPickerTool = new ColorPickerTool(PB_Drawing_Board);
+            textTool = new TextTool(PB_Drawing_Board.CreateGraphics());
         }
 
         public void Select_Color_From_Button(object sender, EventArgs e)
@@ -42,7 +61,11 @@ namespace MDBEditor
             if (Color_Picker.ShowDialog() == DialogResult.OK)
             {
                 selectedButton.BackColor = Color_Picker.Color;
-                //TODO: select color for primary and secondary color
+                switch (selectedButton.Name)
+                {
+                    case nameof(Btn_Primary_Color): primaryColor = Color_Picker.Color; break;
+                    case nameof(Btn_Secondary_Color): secondaryColor = Color_Picker.Color; break;
+                }
             }
         }
 
@@ -54,15 +77,37 @@ namespace MDBEditor
             Lbl_Mouse_Coordinates.Text = e.X + "," + e.Y + " px";
             if (IsMouseDown == true)
             {
-                if(lastPoint != null)
+                switch (currentTool)
                 {
-                    penTool.Loc = e.Location;
-                    penTool.BackColor = Color.Red;
-                    penTool.Size = 3;
-                    penTool.LastPoint = lastPoint;
-                    penTool.Handle();
-                    lastPoint = e.Location;
+                    case DrawingTool.Pen:
+                        if (lastPoint != null)
+                        {
+                            penTool.Loc = e.Location;
+                            penTool.BackColor = primaryColor;
+                            penTool.Size = 3;
+                            penTool.LastPoint = lastPoint;
+                            penTool.Handle();
+                            lastPoint = e.Location;
+                        }
+                        break;
+                    case DrawingTool.Eraser:
+                        if (lastPoint != null)
+                        {
+                            eraserTool.Loc = e.Location;
+                            eraserTool.Size = 10;
+                            eraserTool.LastPoint = lastPoint;
+                            eraserTool.Handle();
+                            lastPoint = e.Location;
+                        }
+                        break;
+                    case DrawingTool.Filler:
+                        fillerTool.BackColor = Color.Blue;
+                        fillerTool.Loc = e.Location;
+                        fillerTool.Handle();
+                        //TODO:Needs Flood Fill Algorithm
+                        break;
                 }
+
             }
         }
 
@@ -102,7 +147,7 @@ namespace MDBEditor
 
         private void Btn_Open_File_Click(object sender, EventArgs e)
         {
-            if(DialogResult.OK == Open_File_Dialog.ShowDialog())
+            if (DialogResult.OK == Open_File_Dialog.ShowDialog())
             {
                 string original_file_name = Open_File_Dialog.SafeFileName.Substring(0, Open_File_Dialog.SafeFileName.LastIndexOf('.'));
                 if (PB_Drawing_Board.IsNullOrEmpty())
@@ -112,7 +157,7 @@ namespace MDBEditor
                 }
                 else
                 {
-                    if(DialogResult.Yes == 
+                    if (DialogResult.Yes ==
                         MessageBox.Show("There are unsaved changes. Do you still want to open this file?",
                         "Unsaved changes",
                         MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
@@ -131,7 +176,7 @@ namespace MDBEditor
         private void CB_Ruler_CheckedChanged(object sender, EventArgs e)
         {
             //Drawing board location is 12,9 -> new location is 32,28
-            if(CB_Ruler.CheckState == CheckState.Checked)
+            if (CB_Ruler.CheckState == CheckState.Checked)
             {
                 PB_Ruler_Left.Visible = true;
                 PB_Ruler_Top.Visible = true;
@@ -149,6 +194,13 @@ namespace MDBEditor
         {
             IsMouseDown = true;
             lastPoint = e.Location;
+            if(currentTool == DrawingTool.Color_Picker)
+            {
+                colorPickerTool.Loc = e.Location;
+                colorPickerTool.Handle();
+                primaryColor = colorPickerTool.DetectedColor;
+                Btn_Primary_Color.BackColor = primaryColor;
+            }
         }
 
         private void PB_Drawing_Board_MouseUp(object sender, MouseEventArgs e)
@@ -156,5 +208,43 @@ namespace MDBEditor
             lastPoint = e.Location;
             IsMouseDown = false;
         }
+
+        /// <summary>
+        /// Get tool name with sender func
+        /// </summary>
+        /// <param name="sender">Any Prologue Tool</param>
+        /// <param name="e">Event Args</param>
+        private void Select_Tool(object sender, EventArgs e)
+        {
+            List<Button> buttonArr = new List<Button>
+            {
+                Btn_Pen,Btn_Erase,Btn_Zoom,Btn_Color_Picker,Btn_Add_Text,Btn_Paint_All
+            };
+            buttonArr.ForEach(p => p.BackColor = SystemColors.Control);
+            Button btn = sender as Button;
+            switch (btn.Name)
+            {
+                case nameof(Btn_Pen):
+                    currentTool = DrawingTool.Pen;
+                    break;
+                case nameof(Btn_Erase):
+                    currentTool = DrawingTool.Eraser;
+                    break;
+                case nameof(Btn_Zoom):
+                    currentTool = DrawingTool.Zoom;
+                    break;
+                case nameof(Btn_Color_Picker):
+                    currentTool = DrawingTool.Color_Picker;
+                    break;
+                case nameof(Btn_Add_Text):
+                    currentTool = DrawingTool.Text;
+                    break;
+                case nameof(Btn_Paint_All):
+                    currentTool = DrawingTool.Filler;
+                    break;
+            }
+            btn.BackColor = SystemColors.ControlLight;
+        }
+
     }
 }

b78fe58ca4d93ac4198851048e165992a4996a05
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-03-20 15:53:37 +0300

    feat: added model class about image information

---
+++ Models/ImageInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace MDBEditor.Models
+{
+    public class ImageInformation
+    {
+        public string Name { get; }
+        public int Resolution { get; }
+
+        public ImageInformation(string name, int resolution)
+        {
+            Name = name;
+            Resolution = resolution;
+        }
+
+        public ImageInformation(int resolution) : this("Unknown", resolution) { }
+    }
+}

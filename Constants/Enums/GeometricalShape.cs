b5a43d0aa61eb7db207c6982b9db0d240d2b8563
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-22 05:17:39 +0300

    feat: added shape enums as constant

---
+++ Constants/Enums/GeometricalShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDBEditor.Constants.Enums
+{
+    public enum GeometricalShape
+    {
+        Left_Arrow,
+        Right_Arrow,
+        Triangle,
+        Left_Triangle,
+        Right_Triangle,
+        Bottom_Triangle,
+        Pentagon,
+        Hexagon,
+        Star,
+        Six_Pointed_Star,
+    }
+}

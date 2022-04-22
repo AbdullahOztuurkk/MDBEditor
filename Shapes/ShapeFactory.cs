321038f3883e069abcfb14e3f8917865bbf304a0
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-22 05:19:09 +0300

    feat: shape factory class has been created

---
+++ Shapes/ShapeFactory.cs
@@ -0,0 +1,36 @@
+﻿using MDBEditor.Constants.Enums;
+using MDBEditor.Extensions;
+using MDBEditor.Shapes.Concrete;
+
+namespace MDBEditor.Shapes
+{
+    public static class ShapeFactory
+    {
+        private static ShapeBase desiredShape;
+        public static ShapeBase GetShapeBase(GeometricalShape shape)
+        {
+            switch (shape)
+            {
+                case GeometricalShape.Left_Arrow:
+                    desiredShape = new LeftArrow();
+                    break;
+                case GeometricalShape.Right_Arrow:
+                    desiredShape = new RightArrow();
+                    break;
+                case GeometricalShape.Triangle:
+                    desiredShape = new Triangle();
+                    break;
+                case GeometricalShape.Pentagon:
+                    desiredShape = new Pentagon();
+                    break;
+                case GeometricalShape.Hexagon:
+                    desiredShape = new Hexagon();
+                    break;
+                case GeometricalShape.Star:
+                    desiredShape = new Star();
+                    break;
+            }
+            return desiredShape;
+        }
+    }
+}

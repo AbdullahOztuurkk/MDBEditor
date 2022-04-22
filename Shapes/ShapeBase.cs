7f8eb903645db1f4b95d8509e6b5689430c4cd76
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-22 05:18:37 +0300

    feat: added abstract class for every shape class

---
+++ Shapes/ShapeBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace MDBEditor.Extensions
+{
+    public abstract class ShapeBase
+    {
+        public abstract Point[] GetCoordinates(Rectangle rect);
+
+        public virtual void Draw(Graphics graphics, Rectangle rect, Pen pen) => graphics.DrawPolygon(pen, GetCoordinates(rect));
+        public virtual void Fill(Graphics graphics, Rectangle rect, Brush brush) => graphics.FillPolygon(brush, GetCoordinates(rect));
+    }
+}

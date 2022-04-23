5334551ec132a1fd71e8d9c3b332e6376df50da7
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-23 18:49:26 +0300

    feat: shape abstract class has been added

---
+++ Shapes/Concrete/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace MDBEditor.Shapes.Concrete
+{
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics graphics, Rectangle rect, Pen pen);
+        public abstract void Fill(Graphics graphics, Rectangle rect, Brush brush);
+
+    }
+}

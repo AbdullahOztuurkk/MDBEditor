8a77a81bb1faecc3476f480b5c62e9007fa832aa
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-23 18:48:33 +0300

    refactor: base shape class has been updated

---
+++ Shapes/RegularShapeBase.cs
@@ -0,0 +1,18 @@
+﻿using MDBEditor.Shapes.Concrete;
+using System.Drawing;
+
+namespace MDBEditor.Shapes
+{
+    public abstract class RegularShapeBase: Shape
+    {
+        public abstract Point[] GetCoordinates(Rectangle rect);
+        public override void Draw(Graphics graphics, Rectangle rect, Pen pen)
+        {
+            graphics.DrawPolygon(pen, GetCoordinates(rect));
+        }
+        public override void Fill(Graphics graphics, Rectangle rect, Brush brush) 
+        {
+            graphics.FillPolygon(brush, GetCoordinates(rect));
+        }
+    }
+}

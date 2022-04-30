def570528aa0d06d03aa5a35a5f23191e717b616
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-04-30 21:29:35 +0300

    refactor: shape factory has been updated entirely

--- Shapes/ShapeFactory.cs
+++ Shapes/ShapeFactory.cs
@@ -1,59 +1,22 @@
 ﻿using MDBEditor.Constants.Enums;
 using MDBEditor.Shapes.Concrete;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 
 namespace MDBEditor.Shapes
 {
     public static class ShapeFactory
     {
-        private static IShape desiredShape;
         public static IShape GetShapeBase(GeometricalShape shape)
         {
-            switch (shape)
-            {
-                case GeometricalShape.Left_Arrow:
-                    desiredShape = new LeftArrow();
-                    break;
-                case GeometricalShape.Right_Arrow:
-                    desiredShape = new RightArrow();
-                    break;
-                case GeometricalShape.Triangle:
-                    desiredShape = new Triangle();
-                    break;
-                case GeometricalShape.Pentagon:
-                    desiredShape = new Pentagon();
-                    break;
-                case GeometricalShape.Hexagon:
-                    desiredShape = new Hexagon();
-                    break;
-                case GeometricalShape.Star:
-                    desiredShape = new Star();
-                    break;
-                case GeometricalShape.Six_Pointed_Star:
-                    desiredShape = new SixPointedStar();
-                    break;
-                case GeometricalShape.Rounded_Rectangle:
-                    desiredShape = new RoundedRectangle();
-                    break;
-                case GeometricalShape.Parallelogram:
-                    desiredShape = new Parallelogram();
-                    break;
-                case GeometricalShape.Trapezoid:
-                    desiredShape = new Trapezoid();
-                    break;
-                case GeometricalShape.Rectangle:
-                    desiredShape = new Rectangle();
-                    break;
-                case GeometricalShape.Circle:
-                    desiredShape = new Circle();
-                    break;
-                case GeometricalShape.Diamond:
-                    desiredShape = new Diamond();
-                    break;
-                case GeometricalShape.Cube:
-                    desiredShape = new Cube();
-                    break;
-            }
-            return desiredShape;
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var type = assembly.GetTypes()
+                .First(t => t.Name == shape.ToString());
+
+            return (IShape)Activator.CreateInstance(type);
         }
     }
 }

d69ef2f096981c0b58be83e4ab38cfa20a745793
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-05-02 23:54:23 +0300

    refactor: added left arrow and right arrow icons to its own buttons

--- Constants/Enums/GeometricalShape.cs
+++ Constants/Enums/GeometricalShape.cs
@@ -25,5 +25,6 @@ namespace MDBEditor.Constants.Enums
         Rectangle,
         Circle,
         Cube,
+        Square,
     }
 }
---
+++ Shapes/Concrete/Square.cs
@@ -0,0 +1,25 @@
+﻿using MDBEditor.Shapes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDBEditor.Shapes.Concrete
+{
+    public class Square : IShape
+    {
+        public void Draw(Graphics graphics, System.Drawing.Rectangle rect, Pen pen)
+        {
+            int size = rect.Width > rect.Height ? rect.Height : rect.Width;
+            graphics.DrawRectangle(pen, new System.Drawing.Rectangle(rect.X, rect.Y, size, size));
+        }
+
+        public void Fill(Graphics graphics, System.Drawing.Rectangle rect, Brush brush)
+        {
+            int size = rect.Width > rect.Height ? rect.Height : rect.Width;
+            graphics.FillRectangle(brush, new System.Drawing.Rectangle(rect.X, rect.Y, size, size));
+        }
+    }
+}

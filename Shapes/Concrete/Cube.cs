ac58168c1e1599e2cfdf30fd659a8dec1c69ccc9
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-05-03 00:18:02 +0300

    feat: cube shape has been added

--- Shapes/Concrete/Cube.cs
+++ Shapes/Concrete/Cube.cs
@@ -8,17 +8,33 @@ namespace MDBEditor.Shapes.Concrete
     {
         public Point[] GetCoordinates(System.Drawing.Rectangle rect)
         {
-            return new Point[] { };
+            int size = rect.Width > rect.Height ? rect.Height : rect.Width;
+            return new Point[] 
+            {
+                //Same as Hexagon
+                new Point(rect.Left + (size / 2 ), rect.Top) ,
+                new Point(rect.Left, (int)( rect.Top + (size * 0.25) )) ,
+                new Point(rect.Left, (int)( rect.Top + (size * 0.75) )) ,
+                new Point(rect.Left +(size / 2 ), rect.Top + size) ,
+                new Point(rect.Left + size, (int)( rect.Top +(size * 0.75) )) ,
+                new Point(rect.Left + size, (int)( rect.Top +(size * 0.25) )) ,
+            };
         }
 
         public void Draw(Graphics graphics, System.Drawing.Rectangle rect, Pen pen)
         {
-            throw new NotImplementedException();
+            int size = rect.Width > rect.Height ? rect.Height : rect.Width;
+            Point centerPoint = new Point(rect.Left + ( size / 2 ), rect.Top + ( size / 2 ));
+            graphics.DrawPolygon(pen, GetCoordinates(rect));
+            //Middle lines
+            graphics.DrawLine(pen, centerPoint, new Point(rect.Left + size, (int)( rect.Top + ( size * 0.25 ) )));
+            graphics.DrawLine(pen, centerPoint, new Point(rect.Left + (size /2), rect.Top + size));
+            graphics.DrawLine(pen, centerPoint, new Point(rect.Left, (int)( rect.Top + (size * 0.25) )));
         }
 
         public void Fill(Graphics graphics, System.Drawing.Rectangle rect, Brush brush)
         {
-            throw new NotImplementedException();
+            graphics.FillPolygon(brush, GetCoordinates(rect));
         }
     }
 }

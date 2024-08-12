e1d29c462a22c4fa2d9508fd01d4270c7eb379b0
Abdullah Öztürk <abdullahozturk99@gmail.com>
2024-08-12 16:58:42 +0300

    feat: filler tool has been added with all functionality

---
+++ src/Tools/Common/IFillerTool.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace MDBEditor.Tools.Common;
+
+public interface IFillerTool : ITool
+{
+    public Color ReplacementColor { get; set; } 
+    public Color TargetColor { get; set; }
+    public Point Location { get; set; }
+
+}
--- src/Tools/Concrete/FillerTool.cs
+++ src/Tools/Concrete/FillerTool.cs
@@ -1,21 +1,50 @@
-﻿using MDBEditor.Tools.Interfaces;
-using System.Drawing;
+﻿using System.Collections.Generic;
 
 namespace MDBEditor.Tools.Concrete;
 
 public class FillerTool : IFillerTool
 {
-    public FillerTool(Bitmap imageCanvas)
+    private readonly Bitmap bitmap;
+    private readonly PictureBox _pictureBox;
+    public FillerTool(PictureBox pictureBox)
     {
-        ImageCanvas = imageCanvas;
+        _pictureBox = pictureBox;
+        this.bitmap = (Bitmap)_pictureBox.Image;
     }
-    private readonly Bitmap ImageCanvas;
 
-    public Color BackColor { get; set; }
+    public Color ReplacementColor { get; set; }
+    public Color TargetColor { get; set; }
     public Point Location { get; set; }
 
     public void Handle()
     {
-        //Needs efficiently Flood Fill Algorithm
+        Color currentColor = bitmap.GetPixel(Location.X, Location.Y);
+        if (currentColor.ToArgb() == TargetColor.ToArgb())
+        {
+            return; // No need to fill if the color is already the target color
+        }
+
+        Stack<Point> pixels = new Stack<Point>();
+        pixels.Push(Location);
+
+        while (pixels.Count > 0)
+        {
+            Point a = pixels.Pop();
+            if (a.X < bitmap.Width && a.X > 0 && a.Y < bitmap.Height && a.Y > 0) //make sure we stay within bounds
+            {
+                var pixelColor = bitmap.GetPixel(a.X, a.Y);
+                var pixelColorArgb = bitmap.GetPixel(a.X, a.Y).ToArgb();
+                var targetColorArgb = TargetColor.ToArgb();
+                if (pixelColor == TargetColor || pixelColor.A == 0)
+                {
+                    bitmap.SetPixel(a.X, a.Y, ReplacementColor);
+                    pixels.Push(new Point(a.X - 1, a.Y));
+                    pixels.Push(new Point(a.X + 1, a.Y));
+                    pixels.Push(new Point(a.X, a.Y - 1));
+                    pixels.Push(new Point(a.X, a.Y + 1));
+                }
+            }
+        }
+        _pictureBox.Refresh();
     }
 }
